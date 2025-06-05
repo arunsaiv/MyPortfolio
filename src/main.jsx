c9b37367885ecb0391618e46b4cfa7842c856fc7
Arun Sai <arunsaiv@gmail.com>
2025-06-05 16:22:46 -0600

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/MyPortfolio/"> {/* 👈 important: use your repo name here */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
