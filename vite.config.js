6ea4f52543dc6868e2d79ae063176b4baebc7e85
Arun Sai <arunsaiv@gmail.com>
2025-06-05 16:27:37 -0600

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/MyPortfolio/', // 👈 important: use your repo name here
-});
